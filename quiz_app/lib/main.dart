4e3a2035266fd6793d14103d2e4ac1671de0d9b5
Tarek Alabd <tarekalabd1@gmail.com>
2024-05-04 21:24:45 +0300

    feat: finalized the edits in quiz app

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -15,6 +15,7 @@ class _MyAppState extends State<MyApp> {
   int questionIndex = 0;
   bool showScore = false;
   int score = 0;
+  String? selectedAnswer;
 
   List<Map<String, dynamic>> questionsWithAnswers = [
     {
@@ -44,61 +45,121 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Quiz App'),
-        ),
-        body: Center(
+        body: SafeArea(
           child: Padding(
             padding:
                 const EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
             child: !showScore
                 ? Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
+                      const SizedBox(height: 60),
                       Text(
                         questionsWithAnswers[questionIndex]['question'],
                         style: const TextStyle(
-                          fontSize: 20,
+                          fontSize: 24,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                      const SizedBox(height: 16),
+                      const SizedBox(height: 8),
+                      const Text(
+                        'Answer and get points!',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      const SizedBox(height: 60),
                       Column(
                         children: (questionsWithAnswers[questionIndex]
                                 ['answers'] as List<String>)
                             .map(
-                              (answer) => SizedBox(
-                                width: double.infinity,
-                                child: Padding(
-                                  padding:
-                                      const EdgeInsets.symmetric(vertical: 4.0),
-                                  child: ElevatedButton(
-                                    onPressed: () {
-                                      if (answer ==
-                                          questionsWithAnswers[questionIndex]
-                                              ['correctAnswer']) {
-                                        setState(() {
-                                          score++;
-                                        });
-                                      }
-                                      if (questionIndex <
-                                          questionsWithAnswers.length - 1) {
-                                        setState(() {
-                                          questionIndex++;
-                                        });
-                                      } else {
-                                        setState(() {
-                                          showScore = true;
-                                        });
-                                      }
-                                      debugPrint('Index: $questionIndex');
-                                    },
-                                    child: Text(answer),
+                              (answer) => Padding(
+                                padding: const EdgeInsets.only(bottom: 16.0),
+                                child: InkWell(
+                                  onTap: () {
+                                    setState(() {
+                                      selectedAnswer = answer;
+                                    });
+                                  },
+                                  child: Container(
+                                    width: double.infinity,
+                                    padding: const EdgeInsets.all(16.0),
+                                    decoration: BoxDecoration(
+                                      color: answer == selectedAnswer
+                                          ? Colors.green
+                                          : null,
+                                      border: Border.all(
+                                        color: Colors.grey.withOpacity(0.3),
+                                      ),
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        Icon(
+                                          Icons.add,
+                                          color: answer == selectedAnswer
+                                              ? Colors.white
+                                              : null,
+                                        ),
+                                        const SizedBox(width: 16),
+                                        Text(
+                                          answer,
+                                          style: TextStyle(
+                                            color: answer == selectedAnswer
+                                                ? Colors.white
+                                                : null,
+                                            fontWeight: answer == selectedAnswer
+                                                ? FontWeight.bold
+                                                : null,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
                                   ),
                                 ),
                               ),
                             )
                             .toList(),
+                      ),
+                      const Spacer(),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 50,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            if (selectedAnswer != null) {
+                              if (selectedAnswer ==
+                                  questionsWithAnswers[questionIndex]
+                                      ['correctAnswer']) {
+                                setState(() {
+                                  score++;
+                                });
+                              }
+                              if (questionIndex <
+                                  questionsWithAnswers.length - 1) {
+                                setState(() {
+                                  questionIndex++;
+                                });
+                              } else {
+                                setState(() {
+                                  showScore = true;
+                                });
+                              }
+                              debugPrint('Index: $questionIndex');
+                            } else {
+                              // Show a snackbar => Task 1
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black,
+                            foregroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                          child: const Text('Next'),
+                        ),
                       ),
                     ],
                   )
@@ -116,6 +177,7 @@ class _MyAppState extends State<MyApp> {
                             questionIndex = 0;
                             score = 0;
                             showScore = false;
+                            selectedAnswer = null;
                           });
                         },
                         child: const Text('Reset'),
